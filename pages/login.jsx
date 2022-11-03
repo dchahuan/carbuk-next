201507f6ac68d781a063d2b1a69f1e84df666579
dchahuan <d.chahuan@uc.cl>
2022-11-03 20:40:42 -0300

    fix: login

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,22 +4,16 @@ import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 // highlight-next-line
 
-import Account from "../components/Account";
-
 const Login = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
   return (
     <div className="container mx-auto ">
-      {!session ? (
-        <Auth
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          theme="dark"
-        />
-      ) : (
-        <h1>You are already logged in</h1>
-      )}
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        theme="dark"
+      />
     </div>
   );
 };
